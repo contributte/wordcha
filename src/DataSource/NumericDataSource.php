9f860b2af28c9cb4bb26ed71e2661630b6c2cd0a
Milan Felix Šulc <rkfelix@gmail.com>
2025-12-26 20:24:23 +0000

    NumericDataSource: add min/max parameters

--- src/DataSource/NumericDataSource.php
+++ src/DataSource/NumericDataSource.php
@@ -2,8 +2,24 @@
 
 namespace Contributte\Wordcha\DataSource;
 
+use Nette\InvalidArgumentException;
+
 class NumericDataSource implements DataSource
 {
+
+	private int $min;
+
+	private int $max;
+
+	public function __construct(int $min = 0, int $max = 10)
+	{
+		if ($min > $max) {
+			throw new InvalidArgumentException(sprintf('Min (%d) must be less than or equal to max (%d)', $min, $max));
+		}
+
+		$this->min = $min;
+		$this->max = $max;
+	}
 
 	public function get(): Pair
 	{
@@ -18,7 +34,7 @@ class NumericDataSource implements DataSource
 
 	private function generateNumber(): int
 	{
-		return random_int(0, 10);
+		return random_int($this->min, $this->max);
 	}
 
 }
